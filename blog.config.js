337211062804a79edeed19599e9b23c4ca4f4100
Fauzi <13282094+fauziabd@users.noreply.github.com>
2022-05-01 00:42:27 +0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'Fauzi',
+  author: 'Fauzi',
+  email: 'fauzi@4bd.co',
+  link: 'https://fauzi.4bd.co',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
